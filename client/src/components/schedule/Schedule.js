b2b4e9a616bf38b22af363a247b9a06935ecc8af
jdhime7972 <79326408+jdhime7972@users.noreply.github.com>
2021-03-21 19:10:23 -0400

    RIS-10 Schedule page

--- client/src/components/schedule/Schedule.js
+++ client/src/components/schedule/Schedule.js
@@ -1,47 +1,51 @@
-import axios from 'axios';
-import React, {useState, useEffect} from 'react';
-import {ListGroup, ListGroupItem} from 'react-bootstrap';
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import { ListGroup, ListGroupItem } from "react-bootstrap";
 
-function Schedule  () {
+function Schedule() {
   const [state, setState] = useState([
     {
       id: "",
       firstName: "",
       lastName: "",
-      time: "",
       date: "",
+      time: "",
       physician: "",
     },
-  ])
-  
-    useEffect(() => {
-      axios.get("/api/schedule", state)
-      .then(res => {
-        console.log(res)
-        setState(res.data)
-      }) 
-      .catch(err => {
-        console.log(err)
-      })  
-    }, [])
-  const appointmentList = state.map(appointment => (
+  ]);
+
+  useEffect(() => {
+    axios
+      .get("/api/schedule")
+      .then((res) => {
+        console.log(res);
+        setState(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  const appointmentList = state.map((appointment) => (
     <ListGroup horizontal>
-      <ListGroupItem action href = "/patient/:id">{appointment.firstName} {appointment.lastName}</ListGroupItem> 
+      <ListGroupItem action href="/patient/:id">
+        {appointment.firstName} {appointment.lastName}
+      </ListGroupItem>
       <ListGroupItem>{appointment.date}</ListGroupItem>
-      <ListGroupItem>{appointment.time}</ListGroupItem>
       <ListGroupItem>{appointment.physician}</ListGroupItem>
-      <ListGroupItem action href = "appointments/:id">edit</ListGroupItem>
-      <ListGroupItem action href = "/api/appointment/delete/:id" METHOD DELETE>delete</ListGroupItem> 
-      <ListGroupItem action href = "/api/appointment/checkin/:id" METHOD POST>check-in</ListGroupItem>
-      <ListGroupItem action href = "/api/appointment/checkin/:id" METHOD POST>check-out</ListGroupItem> 
-    </ListGroup>))
+      <ListGroupItem>
+        <a>edit</a>
+        <a>delete</a>
+        {appointment.status === "Not Started" ? <a>check-in</a> : <a>check-out</a>}
+      </ListGroupItem>
+    </ListGroup>
+  ));
+  
   return (
     <div>
-      <div className = "schedule">
-        {appointmentList}
-      </div>
+      <div className="schedule">{appointmentList}</div>
     </div>
   );
-};
+}
 
 export default Schedule;
